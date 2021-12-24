3b4e4fd58e0ed497202348f577118d00c0e57e0b
yasminfathimaa9 <yasminfathimaa9@gmail.com>
2021-12-24 21:30:20 +0530

    commit

--- src/HeaderImg.js
+++ src/HeaderImg.js
@@ -1,7 +1,7 @@
 function HeaderImg(props){
     return(
 <div>
-    <img className="images"src={props.src} />
+    <img className="images"src={props.src} alt="ima" />
     <p className="size">{props.name}</p>
     <p className="id">{props.id}</p>
 </div>
